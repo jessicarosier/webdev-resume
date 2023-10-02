515a0aa3320e12492792fc573e4d96f23e690736
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-10-01 19:36:41 -0500

    init

---
+++ js/main.js
@@ -0,0 +1,41 @@
+//TODO: style/format project cards
+//TODO: add project descriptions to cards, on hover overlay slides up to reveal description
+//TODO: add project demo links to cards, on hover overlay slides up to reveal demo link
+
+
+projects = [
+    {
+        name: "Favorite Movies App",
+        description: "This web app allows users to search for movies and add them to a list of favorites. The app uses the TMDB API to search for movies and retrieve movie data as well as a local JSON database to store the list of favorite movies. This was a pair programming development project completed in 1 week. Check out my partner's GitHub profile here",
+        demo: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        github: "https://github.com/jessicarosier/favorite-movies-app",
+    },
+
+    {
+        name: "Tic Tac Toe",
+        description: "This is a simple tic tac toe game that I created using HTML, CSS, and JavaScript. The game allows two players to play against each other and keeps track of the score. This project was completed in 3 days.",
+        demo: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        github: "https://github.com/jessicarosier/tic-tac-toe"
+    },
+
+    {
+        name: "Weather Map App",
+        description: "This is a simple weather app that I created using HTML, CSS, and JavaScript. The app allows users to search for a city and displays the current weather conditions as well as a 5 day forecast. This project was completed in 5 days.",
+        demo: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        github: ""
+    },
+
+    {
+        name: "Calculator",
+        description: "This is a simple calculator that I created using HTML, CSS, and JavaScript. The calculator allows users to perform basic math operations. This project was completed in 3 days.",
+        demo: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        github: "https://github.com/jessicarosier/calculator"
+    },
+
+    {
+        name: "Coffee Shop Website",
+        description: "This is a simple website for a coffee shop that I created using HTML, CSS, and JavaScript. This project was completed in 2 days.",
+        demo: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        github: ""
+    }
+];
