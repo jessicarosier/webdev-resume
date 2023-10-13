e14ad4714ba3063b6079f2a79ba0f184a71a9ec8
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-10-12 21:07:19 -0500

    added live site url to projects object

--- js/main.js
+++ js/main.js
@@ -28,34 +28,39 @@ let projects = [
         description: "This web app allows users to search for movies and add them to a list of favorites. The app uses the TMDB API to search for movies and retrieve movie data as well as a local JSON database to store the list of favorite movies. This was a pair programming development project completed in 1 week.",
         demo: "video/fav-movie-app-demo.mp4",
         github: "https://github.com/jessicarosier/favorite-movies-app",
+        liveSite: ""
     },
 
     {
         name: "Weather Map",
         description: "This is a weather app created using HTML, CSS, and JavaScript. The app uses the OpenWeatherMap API to retrieve weather data for a given location as well as the MapBox API to display a map of the location. This project was completed in 3 days.",
         demo: "video/weather-map-demo.mp4",
-        github: ""
+        github: "https://github.com/jessicarosier/weather-map",
+        liveSite: "https://weather-map.jessicarosier.com/"
     },
 
     {
         name: "Tic Tac Toe",
         description: "This is a simple tic tac toe game created using HTML, CSS, and JavaScript. The game allows two players to play against each other and keeps track of the score. This project was completed in 3 days.",
         demo: "video/tic-tac-toe-demo.mp4",
-        github: "https://github.com/jessicarosier/tic-tac-toe"
+        github: "https://github.com/jessicarosier/tic-tac-toe",
+        liveSite: ""
     },
 
     {
         name: "Calculator",
         description: "This is a simple calculator created using HTML, CSS, JavaScript and jQuery. The calculator allows users to perform basic math operations. This project was completed in 3 days.",
         demo: "video/calculator-demo.mp4",
-        github: "https://github.com/jessicarosier/calculator"
+        github: "https://github.com/jessicarosier/calculator",
+        liveSite: ""
     },
 
     {
         name: "Coffee Project",
         description: "This is a simple website for a coffee shop created using HTML, CSS, and JavaScript. This was a pair programming development project completed in 1 week.",
         demo: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-        github: ""
+        github: "",
+        liveSite: ""
     }
 ];
 
