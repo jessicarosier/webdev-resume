dcb72a5f1f64f14a136204d7b646cfc8ca79ad1d
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-10-27 20:43:23 -0500

    added image loop to about me

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-console.log("Hello there fellow code warrior! This site is currenlty under construction :)");
+console.log("Hello there fellow code warrior! This site is currently under construction :)");
 
+//
 let aboutMe = ["Full Stack Web Developer 👩🏽‍💻", "U.S Navy Veteran 🇺🇸⚓️", "Coffee Lover ☕️", "Traveler ✈️", "Lifelong Learner 📚", "Problem Solver 🧐", "Technology Enthusiast 💻"];
 
 let aboutMeDisplay = document.querySelector("#about-me");
 let currentIndex = 0;
 
+//loops through the aboutMe array and displays each string in the aboutMeDisplay for 2 seconds, loops indefinitely
 function renderAboutMe() {
     if (currentIndex < aboutMe.length) {
         aboutMeDisplay.innerHTML = aboutMe[currentIndex];
@@ -12,10 +14,10 @@ function renderAboutMe() {
     } else {
         currentIndex = 0;
     }
-
     setTimeout(renderAboutMe, 2000);
 }
 
+//calls the renderAboutMe function
 renderAboutMe();
 
 
@@ -27,7 +29,6 @@ let projects = [
         github: "https://github.com/jessicarosier/favorite-movies-app",
         liveSite: "https://movies-app.jessicarosier.com"
     },
-
     {
         name: "Weather Map",
         description: "This is a weather app created using HTML, CSS, and JavaScript. The app uses the OpenWeatherMap API to retrieve weather data for a given location as well as the MapBox API to display a map of the location. This project was completed in 3 days.",
@@ -35,23 +36,6 @@ let projects = [
         github: "https://github.com/jessicarosier/weather-map",
         liveSite: "https://weather-map.jessicarosier.com/"
     },
-
-    // {
-    //     name: "Tic Tac Toe",
-    //     description: "This is a simple tic tac toe game created using HTML, CSS, and JavaScript. The game allows two players to play against each other and keeps track of the score. This project was completed in 3 days.",
-    //     demo: "video/tic-tac-toe-demo.mp4",
-    //     github: "https://github.com/jessicarosier/tic-tac-toe",
-    //     liveSite: ""
-    // },
-    //
-    // {
-    //     name: "Calculator",
-    //     description: "This is a simple calculator created using HTML, CSS, JavaScript and jQuery. The calculator allows users to perform basic math operations. This project was completed in 3 days.",
-    //     demo: "video/calculator-demo.mp4",
-    //     github: "https://github.com/jessicarosier/calculator",
-    //     liveSite: ""
-    // },
-
     {
         name: "Coffee Project",
         description: "This is a simple website for a coffee shop created using HTML, CSS, and JavaScript. Uses local storage to make data persistent. This was a pair programming development project completed in 1 week.",
@@ -62,19 +46,41 @@ let projects = [
 ];
 
 
+let images = [
+    "img/usn.jpeg",
+    "img/lulumahu_falls.jpeg",
+    "img/jess3.jpeg"
+];
+
+
+let currentImgIndex = 0;
+const currentImg = document.getElementById("current-img");
+
+//loop through the images array and changes the image displayed in the About Me section every 5 seconds
+function changeImage() {
+    currentImg.style.opacity = 0;
+    setTimeout(function () {
+        currentImg.src = images[currentImgIndex];
+        currentImgIndex = (currentImgIndex + 1) % images.length;
+        currentImg.style.opacity = 1;
+    }, 1000); // sets the opacity to 1 after 1 second
+}
+
+// calls the changeImage function
+setInterval(changeImage, 5000); // changes the image every 5 seconds
+changeImage();
+
+
+//builds the project cards
 function renderProjects(project) {
     let projectCard = document.createElement("div");
     projectCard.classList.add("project-card");
     projectCard.innerHTML = `
-<h3>${project.name}</h3>
+    <h3>${project.name}</h3>
     <a href="${project.github}" target="_blank"><img src="img/github.svg" class="git-img"></a>
-
       <p>${project.description}</p>
-
       <a class="button" href="${project.liveSite}" target="_blank">  Live Site  </a>`;
-
     document.querySelector(".projects-container").appendChild(projectCard);
-
 }
 
 projects.forEach((project) => {
